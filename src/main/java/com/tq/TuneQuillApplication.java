acb1bafd026bd6d12a6284f9b25e30e66746accf
Nabeel Ahmed Mohammad <nabeelahmed08pro@gmail.com>
2024-07-02 23:41:38 -0500

    Initial commit

---
+++ src/main/java/com/tq/TuneQuillApplication.java
@@ -0,0 +1,13 @@
+package com.tq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneQuillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneQuillApplication.class, args);
+	}
+
+}
